e539c33985d04d81e0fb5b0bdb742cbb848cd474
liuling <382524176@qq.com>
2023-07-26 00:44:31 +0800

    initial commit-day02 data

---
+++ month01/day02/huilvzhuanhuanqi.py
@@ -0,0 +1,10 @@
+"""
+        汇率转换器
+"""
+# 1、输入美元
+usd = float(input("请输入美元"))
+# 2、运算
+cny = 6.405 * usd
+# 3、返回：xxx美元=yyy人民币
+print(str(usd) + "美元=" + str(cny) + "人民币")
+
---
+++ month01/day02/l06变量创建的三种方式.py
@@ -0,0 +1,12 @@
+# 1、创建一个变量，指向一份数据
+name01 = "刘凌"
+print(name01)
+
+# 2、创建多个变量，分别指向多个数据
+name02, name03 = "刘朝闻", "刘峻熙"
+print(name03)
+
+# 3、创建多个变量，指向同一份数据
+name04 = name05 = name06 = "黄肖斌"
+print("name05")
+print(name05)
